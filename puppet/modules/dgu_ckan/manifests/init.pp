a4ec50f86ecdd3029e7903d121e3ddb32a37c0ba
Ross Jones <ross@servercode.co.uk>
2013-09-24 08:59:01 +0100

    Added logreporter and the cron file

--- puppet/modules/dgu_ckan/manifests/init.pp
+++ puppet/modules/dgu_ckan/manifests/init.pp
@@ -116,6 +116,7 @@ class dgu_ckan {
     'ckanext-ga-report',
     'ckanext-datapreview',
     'ckanext-importlib',
+    'logreporter',
   ]
   dgu_ckan::pip_package { $pip_pkgs_local:
     require => Python::Virtualenv[$ckan_virtualenv],
--- src/git_clone_all.sh
+++ src/git_clone_all.sh
@@ -3,48 +3,48 @@
 # git clone steps
 # ---------------
 
-git clone	git@github.com:datagovuk/ckan 
+git clone	git@github.com:datagovuk/ckan
 cd ckan
 git checkout release-v2.0.1-dgu
 cd -
 
-git clone	git@github.com:datagovuk/ckanext-archiver 
+git clone	git@github.com:datagovuk/ckanext-archiver
 cd ckanext-archiver
 git checkout master
 cd -
 
-git clone	git@github.com:datagovuk/ckanext-datapreview 
+git clone	git@github.com:datagovuk/ckanext-datapreview
 cd ckanext-datapreview
 git checkout master
 cd -
 
-git clone	git@github.com:datagovuk/ckanext-dgu 
+git clone	git@github.com:datagovuk/ckanext-dgu
 cd ckanext-dgu
 git checkout 2.0
 ln -s ../commit-msg.githook ./.git/hooks/commit-msg
 cd -
 
-git clone	git@github.com:datagovuk/ckanext-ga-report 
+git clone	git@github.com:datagovuk/ckanext-ga-report
 cd ckanext-ga-report
 git checkout master
 cd -
 
-git clone	git@github.com:datagovuk/ckanext-harvest 
+git clone	git@github.com:datagovuk/ckanext-harvest
 cd ckanext-harvest
 git checkout 2.0
 cd -
 
-git clone	git@github.com:datagovuk/ckanext-os 
+git clone	git@github.com:datagovuk/ckanext-os
 cd ckanext-os
 git checkout master
 cd -
 
-git clone	git@github.com:datagovuk/ckanext-qa 
+git clone	git@github.com:datagovuk/ckanext-qa
 cd ckanext-qa
 git checkout 2.0
 cd -
 
-git clone	git@github.com:datagovuk/ckanext-spatial 
+git clone	git@github.com:datagovuk/ckanext-spatial
 cd ckanext-spatial
 git checkout dgu
 cd -
@@ -56,6 +56,11 @@ cd -
 
 git clone	git@github.com:datagovuk/dgu-joint-assets
 cd dgu-joint-assets
+git checkout master
+cd -
+
+git clone   git@github.com:datagovuk/logreporter.git
+cd logreporter
 git checkout master
 cd -
 
