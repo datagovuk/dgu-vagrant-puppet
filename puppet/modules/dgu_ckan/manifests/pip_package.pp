4d9faee9a542335b1071dce49dc0e9045883b659
Vladimir Mašala <vmasala@omega-software.hr>
2014-09-16 10:48:26 +0200

    Revert "https://github.com/datagovuk/dgu-vagrant-puppet/issues/17"
    This reverts commit 7240641124a5ae0eeda569ac1f0d285e30bf402e.

--- puppet/modules/dgu_ckan/manifests/pip_package.pp
+++ puppet/modules/dgu_ckan/manifests/pip_package.pp
@@ -16,7 +16,7 @@ define dgu_ckan::pip_package ($ensure = present, $owner, $local) {
     present: {
       if !($grep in $ckan_pip_freeze) {
         exec { "pip_install_${name}":
-          command     => "${ckan_virtualenv}/bin/pip install –pre --no-index --find-links=file:///vagrant/pypi --log-file ${ckan_virtualenv}/pip.log ${url}",
+          command     => "${ckan_virtualenv}/bin/pip install --no-index --find-links=file:///vagrant/pypi --log-file ${ckan_virtualenv}/pip.log ${url}",
           user        => $owner,
           logoutput   => "on_failure",
         }
